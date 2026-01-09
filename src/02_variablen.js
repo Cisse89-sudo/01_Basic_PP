6e2a60ecd80f43443ce144a159e7a3716a0bd36d
Christian Isse <C.Huwe89@hotmail.de>
2026-01-09 11:55:20 +0000

    Deklaration + Wertzuweisung

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -10,6 +10,17 @@
 
 /***** Variablen 01 *******/
 
+// console.log("hi");
+// console.log(hi);
+
+/* Deklaration + Wertzuweisung I */
+
+let firstName; // Deklaration (Definition)
+ console.log(firstName); // Ausgabe
+firstName = "Max"; // Wertzuweisung
+ console.log(firstName); // Ausgabe
+let familyName = "Mütze"; // Deklaration + Wertzuweisung
+ console.log("Hi, ich bin " + firstName + " " + familyName +"!"); // Ausgabe
 
 
 /* Deklaration + Wertzuweisung I */
