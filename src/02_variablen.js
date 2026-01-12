0aabf7fa8a45d0944305676f51ff4f07bbf39647
Christian Isse <C.Huwe89@hotmail.de>
2026-01-12 09:43:34 +0000

    0.2  Variablen vs. Konstanten in JS

--- src/02_variablen.js
+++ src/02_variablen.js
@@ -15,15 +15,53 @@
 
 /* Deklaration + Wertzuweisung I */
 
-let firstName; // Deklaration (Definition)
- console.log(firstName); // Ausgabe
-firstName = "Max"; // Wertzuweisung
- console.log(firstName); // Ausgabe
-let familyName = "Mütze"; // Deklaration + Wertzuweisung
- console.log("Hi, ich bin " + firstName + " " + familyName +"!"); // Ausgabe
+// let firstName; // Deklaration (Definition)
+//  console.log(firstName); // Ausgabe
+// firstName = "Max"; // Wertzuweisung
+//  console.log(firstName); // Ausgabe
+// let familyName = "Mütze"; // Deklaration + Wertzuweisung
+//  console.log("Hi, ich bin " + firstName + " " + familyName +"!"); // Ausgabe
 
 
 /* Deklaration + Wertzuweisung I */
+
+
+
+
+// let test; // Deklaration 
+
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
+
+// test= "hi"
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
+
+// test= 2;
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
+
+
+// test= true;
+// console.log("Datentyp: " + typeof test); // Ausgabe Datentyp
+// console.log("Inhalt: " + test); // Ausgabe Inhalt
+// console.log("----------------")
+
+/* Variablen vs. Konstanten in JS */
+
+ // Variable
+let test;  // Variable : Deklaration
+test = "hi"; // Wertzuweisung (Initialisierung)
+test = "hello"; // Überschreiben
+console.log("Inhalt: " + test); // Ausgabe
+
+// Konstante
+const test2 = "hi!";
+
+console.log("Inhalt: " + test2); // Ausgabe
 
 
 
