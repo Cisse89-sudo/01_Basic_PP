46724c5c13dbd15752a3e002fb4235de07253ba8
Christian Isse <C.Huwe89@hotmail.de>
2026-01-15 11:01:04 +0000

    0.8.5  03a. Vorbereitung -Trennen der Verantwortlichkeiten

--- src/08_funktionen_I.js
+++ src/08_funktionen_I.js
@@ -29,9 +29,44 @@ function ausgabeNamen() {
 
 // 2b. Parametrisierung + Datenübergabe von AUSSEN
 
-ausgabeNamen2();
-ausgabeNamen2("Yasin") // Argumente --> Daten für Parameter
+// ausgabeNamen2();
+// ausgabeNamen2("Yasin") // Argumente --> Daten für Parameter
+// ausgabeNamen2("Tarik") // ausgabeNamen2("Sandra")
 
 function ausgabeNamen2(firstName) {
     console.log("Hallo, " + firstName  + "!");
     }
+
+
+    /***** Funktionen 02c *****/
+// 2c. Mehrere Parameter / Argumente
+
+// ausgabeNamen3();
+// ausgabeNamen3("Mad", "Max");
+
+// Argumente werden von prompt() geliefert --> Input
+// const prompt = require('prompt-sync')({sigint: true});
+// ausgabeNamen3(prompt("Vorname: "),prompt("Name: "));
+
+function ausgabeNamen3(firstName, familyName) {  // Parameter
+    console.log("Hallo, " + firstName + " " + familyName + "!");
+}
+
+/***** Funktionen 03a *****/
+// 03a. Vorbereitung -Trennen der Verantwortlichkeiten
+// Postulat: one function = one job (uncle Bob)
+// SRP single responsibility principle
+
+ausgabeNamenSRP("Mad", "Max");
+
+function ausgabeNamenSRP(firstName, familyName) {  // Parameter
+
+     //1st responsibility: string composing
+     const GAP =" ";
+     let outputStr = ("Hallo, " + firstName + GAP + familyName + "!");
+
+     //2nd responsibility: string output
+    console.log(outputStr);
+
+
+}
