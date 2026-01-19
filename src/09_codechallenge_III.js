a37508a54258050be7bc79ca8ca7d91b5028b15f
Christian Isse <C.Huwe89@hotmail.de>
2026-01-19 09:44:10 +0000

    11.0 src/11_arrays_I.js

--- src/09_codechallenge_III.js
+++ src/09_codechallenge_III.js
@@ -11,52 +11,52 @@
 // https://github.com/archive-Basic-P-25-09/02a_Bestellung.git
 
 
-let food  = "";   // ausgewähltes Essen
-let drink = "";   // ausgewähltes Getränk
+// let food  = "";   // ausgewähltes Essen
+// let drink = "";   // ausgewähltes Getränk
 
-function order() {
-  alert("Herzlich Willkommen bei MD, Ihre Bestellung bitte!");
+// function order() {
+//   alert("Herzlich Willkommen bei MD, Ihre Bestellung bitte!");
 
-  orderFood();                      // Funktionsaufruf Essen
+//   orderFood();                      // Funktionsaufruf Essen
 
-  const more = prompt("Möchten Sie ein Getränk dazu bestellen? (j/n)");
-  if (more === "j" || more === "J") {
-    orderDrink();                   // Funktionsaufruf Getränk
-  }
+//   const more = prompt("Möchten Sie ein Getränk dazu bestellen? (j/n)");
+//   if (more === "j" || more === "J") {
+//     orderDrink();                   // Funktionsaufruf Getränk
+//   }
 
-  alert("Hier ist Ihre Bestellung: " + food + (drink ? " und " + drink : ""));
-  alert("Vielen Dank, und auf Wiedersehen!");
-}
+//   alert("Hier ist Ihre Bestellung: " + food + (drink ? " und " + drink : ""));
+//   alert("Vielen Dank, und auf Wiedersehen!");
+// }
 
-function orderDrink() {
-  const choice = prompt("Getränk: 1) Cola, 2) Fanta, 3) Sprite (Zahl eingeben)");
+// function orderDrink() {
+//   const choice = prompt("Getränk: 1) Cola, 2) Fanta, 3) Sprite (Zahl eingeben)");
 
-  if (choice === "1") {
-    drink = "Cola";
-  } else if (choice === "2") {
-    drink = "Fanta";
-  } else if (choice === "3") {
-    drink = "Sprite";
-  } else if (choice === "4") {
-    drink = "Wasser"
-  }
-    alert("Ungültige Eingabe, kein Getränk gewählt.");
-  }
+//   if (choice === "1") {
+//     drink = "Cola";
+//   } else if (choice === "2") {
+//     drink = "Fanta";
+//   } else if (choice === "3") {
+//     drink = "Sprite";
+//   } else if (choice === "4") {
+//     drink = "Wasser"
+//   }
+//     alert("Ungültige Eingabe, kein Getränk gewählt.");
+//   }
 
 
-function orderFood() {
-  const choice = prompt("Burgerauswahl: 1) Hamburger, 2) Cheeseburger, 3) Chiliburger (Zahl eingeben)");
+// function orderFood() {
+//   const choice = prompt("Burgerauswahl: 1) Hamburger, 2) Cheeseburger, 3) Chiliburger (Zahl eingeben)");
 
-  if (choice === "1") {
-    food = "Hamburger";
-  } else if (choice === "2") {
-    food = "Cheeseburger";
-  } else if (choice === "3") {
-    food = "Chiliburger";
-  } else {
-    alert("Ungültige Eingabe, es wird kein Burger bestellt.");
-  }
-}
+//   if (choice === "1") {
+//     food = "Hamburger";
+//   } else if (choice === "2") {
+//     food = "Cheeseburger";
+//   } else if (choice === "3") {
+//     food = "Chiliburger";
+//   } else {
+//     alert("Ungültige Eingabe, es wird kein Burger bestellt.");
+//   }
+// }
 
 
 
