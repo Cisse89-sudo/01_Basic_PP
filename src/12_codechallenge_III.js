9097176f074db211ca37a87803cddbe053e7aa9b
Christian Isse <C.Huwe89@hotmail.de>
2026-01-20 09:07:13 +0000

    14.0 src/14_objects.js

--- src/12_codechallenge_III.js
+++ src/12_codechallenge_III.js
@@ -19,8 +19,46 @@ output(getSentence(["Ich","bin"],"E"));
 output(getSentence(["Ich","hätt","gern","die","Platt","von","dene","zwei","diwodaso","Spass","mache,","habbe","Sie","die"],"Q"));
 
 function getSentence(arr,op) {
-   // ???
+//     const GAP = " ";
+//     const PUNCT =getPunct(op);
+//     let str = "";
+// }
+// for (let i = 0; i < arr.length; i++)
+//     if (i != arr.length-1) {
+//         str += arr[i] + GAP;
+//     }else {
+//         str += arr[i] + PUNCT;
+//     }
+     
+//    function getSentenceArr2(arr) {
+
+//     const GAP = " ";
+//     const PUNCT = ".";
+//     let str = "";
+
+    // // 1. Iteration: Struktur GENAU erzeugen ...
+    for (let i = 0; i < arr.length; i++) {
+        if (i != arr.length - 1) {
+           str += arr[i] + GAP; 
+        } else {
+            str += arr[i] + PUNCT;
+        }
+    }
+    return str;
+
+    // 2. Iteration: Struktur erzeugen + trimmen()
+    // for (let i = 0; i < arr.length; i++) {
+    //     str += arr[i] + GAP; 
+    // }
+    // return str.trim() + PUNCT;
+
+     // 3. Variante: Ausnutzen von generischen Fuktionen ...
+    // return arr.join(GAP) + PUNCT;
+
+
 }
+
+
 
 
 
